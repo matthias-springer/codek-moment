a3b5b5f4351573a3e7fb0ae01af80a1e4f00fcbe
Eddie Totman <edd.totman@gmail.com>
2014-02-06 01:10:50 -0800

    Removed ExpiredPlaceIts file because that requirement was removed (see Griswolds post on Piazza). Also began crafting PlaceIt file I/O

---
+++ src/edu/ucsd/cse110/team27/placeits/data/FileManager.java
@@ -0,0 +1,18 @@
+package edu.ucsd.cse110.team27.placeits.data;
+
+import java.util.List;
+
+public class FileManager {
+	private static final String ACTIVE_FILE_NAME = "ActivePlaceits.txt";
+	private static final String PULLED_DOWN_FILE_NAME = "PulledDownPlaceits.txt";
+	
+	public void storeActivePlaceits(List<PlaceIt> placeits) {
+		
+	}
+	
+	public List<PlaceIt> getActivePlaceits(){ 
+		return null;
+	}
+	
+
+}
